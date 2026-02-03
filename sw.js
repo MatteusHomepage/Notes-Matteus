6b689cc628379489ba9973e42a18b7efde7c95ec
MatteusHomepage <protectorhotel@gmail.com>
2026-02-03 18:39:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,33 @@
-const CACHE="notes-v1"
+const CACHE_NAME = "notes-v1"
 
-self.addEventListener("install",e=>{
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./README.md",
+  "./package.json",
+  "./server.js",
+  "./sw.js"
+]
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(["./notes.html","./"]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   )
 })
 
-self.addEventListener("fetch",e=>{
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  )
+})
+
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   )
 })
